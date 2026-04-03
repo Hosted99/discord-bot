83e8db7b6a797412da26ec9ff24c66972b3dece8
Hosted99 <140538111+Hosted99@users.noreply.github.com>
2026-04-04 00:36:46 +0300

    main.js

--- my-discord-bot/main.js
+++ my-discord-bot/main.js
@@ -16,6 +16,18 @@ const client = new Client({
     ]
 });
 
+
+//  проблема с Render, без да пречи на Railway
+const http = require('http');
+const port = process.env.PORT || 3000;
+http.createServer((req, res) => {
+  res.writeHead(200);
+  res.end('Bot is running!');
+}).listen(port);
+
+console.log(`Monitoring server started on port ${port}`);
+
+
 // Събитие: Ботът е зареден и онлайн
 client.once("clientReady", async () => {
     await initDB(); // Свързване с Neon Postgres
