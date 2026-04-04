8600ee42cc0f47f4bf768be86b05845c3e695c01
Hosted99 <140538111+Hosted99@users.noreply.github.com>
2026-04-04 13:35:48 +0300

    main.js

--- my-discord-bot/main.js
+++ my-discord-bot/main.js
@@ -9,7 +9,7 @@ const { logDeletedMessage } = require("./utilities/logger");
 
 // Инициализация на Gemini AI
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
-const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash-latest" });
+const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 const translationCooldown = new Set(); // ВАЖНО!!!!!!!!
 
 // Инициализация на клиента с всички нужни права
