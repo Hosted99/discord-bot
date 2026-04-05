7eed560ba9e85e39b1ffcd508ae7607a93b08a31
Hosted99 <140538111+Hosted99@users.noreply.github.com>
2026-04-05 16:53:01 +0300

    staticReminders.js

--- my-discord-bot/data/staticReminders.js
+++ my-discord-bot/data/staticReminders.js
@@ -3,5 +3,17 @@ module.exports = [
   { cron: "0 12 * * 2,5", message: "Belly Rush today!", target: "everyone" },
   { cron: "0 12 * * 3,5,0", message: "Mania today!", target: "@everyone" },
   { cron: "45 21 * * 3,5,0", message: "15 min until Mania starts!", target: "@everyone" },
-  { cron: "0 22 * * 3,5,0", message: "Mania is open!", target: "@everyone" }
+  { cron: "0 22 * * 3,5,0", message: "Mania is open!", target: "@everyone" },
+  // 1. Повтаря се на всеки 2 часа от 12:00 до 18:00 (12, 14, 16, 18 часа)
+{ cron: "0 12-18/2 * * 2,5", message: "💰 Belly Rush is active! Go get those berries!", target: "@everyone" },
+
+// 2. Специално съобщение само за финала в 20:00
+{ cron: "0 20 * * 2,5", message: "🚨 Belly Rush is ending soon! This is your LAST CHANCE!", target: "@everyone" },
+  // 1. Повтаря се на всеки 5 минути (15:00, 15:05, 15:10)
+{ cron: "0,5,10 15 * * *", message: "🧪 TEST: Ремайндър на всеки 5 минути! (Ongoing)", target: "@everyone" },
+
+// 2. Финално съобщение точно в 15:15
+{ cron: "15 15 * * *", message: "🏁 FINAL TEST: Времето изтече! Това беше последното съобщение.", target: "@everyone" }
+
+
 ];
