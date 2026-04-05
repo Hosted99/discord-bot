ab7f5485cc43d21bf75e749cfe51d2bb08b76aa7
Hosted99 <140538111+Hosted99@users.noreply.github.com>
2026-04-05 13:24:42 +0300

    repairMessages.js

--- my-discord-bot/data/repairMessages.js
+++ my-discord-bot/data/repairMessages.js
@@ -20,12 +20,4 @@ module.exports = {
         "🛡️ Бронята на **{user}** е подсилена срещу морски чудовища!",
         "⚡ Светкавичен ремонт за **{user}**! Вятърът е във вашите платна!"
     ]
-};module.exports = [
-    "🔧 Knock-knock! **{user}** is now as good as new... well, at least it’s not sinking!",
-    "🛠️ Repair complete! **{user}**, try to avoid the reefs next time!",
-    "🚢 **{user}** has been polished to a shine! Ready for new adventures!",
-    "🧪 A bit of duct tape, a little magic, and **{user}** is ready to sail again!",
-    "⚓ The captain of **{user}** can rest easy – the holes are plugged (mostly)!",
-    "🧹 Brand new look for **{user}**! Careful, the paint is still wet!",
-    "⚡ Lightning-fast repair for **{user}**! It's now faster than the wind!"
-];
+};
