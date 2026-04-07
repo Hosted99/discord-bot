320eeb73e20d68548216ec5768d6bfe6e81c8d66
Hosted99 <140538111+Hosted99@users.noreply.github.com>
2026-04-07 20:14:53 +0300

    staticReminders.js

--- my-discord-bot/data/staticReminders.js
+++ my-discord-bot/data/staticReminders.js
@@ -1,9 +1,58 @@
+// Функция за случаен избор на съобщение
+const random = (arr) => arr[Math.floor(Math.random() * arr.length)];
+
+// PREPARATION MESSAGES (11:00)
+const warHype = [
+  "⚔️ 'We're going to be the Pirate Kings!' - Prep has started! Let's sharpen our blades like Zoro! 🗡️",
+  "🍖 Our feast is over! The crew is ready, are you? Set our course for the Guild War! 🏴‍☠️",
+  "🛡️ 'Our dreams... HAVE NO END!' - Get your gear ready, guild preparation is LIVE! 🌊",
+  "⚓ Raise the sails! We need everyone ready before the storm hits. Preparation starts now! 🧭",
+  "🔥 Focus up, Nakama! We're not just a team, we're a legend in the making. Get ready! 🛡️"
+];
+
+// BATTLE MESSAGES (12, 15, 18:00)
+const warBattle = [
+  "🔥 OUR GUM-GUM BATTLE IS ON! Guild War is LIVE! Let's go beyond our limits, GEAR SECOND! ⚡",
+  "🏹 'The One Piece is real!' - Show them our power! Don't let them touch our treasure! 💎",
+  "💥 'Scars on our backs are a swordsman's shame!' - To the front lines, we never run away! 🤜",
+  "🌩️ We're bringing the 'D' energy to the battlefield! Charge together and crush them! ⚡",
+  "🏴‍☠️ The Grand Line belongs to US! Join the fight and show them the strength of our crew! 🔱"
+];
+
+// END MESSAGES (21:00)
+const warEnd = [
+  "🍻 'Inherited Will, The Destiny of the Age!' - Our war has ended, let's party like pirates! 🍖",
+  "🏆 We're not just crewmates, we're FAMILY! Great job today, legends. Rest up for the next voyage! ⚓",
+  "🌊 The dust has settled on the battlefield. We stood our ground! Time to head to the tavern! 🍻",
+  "⭐ Another victory (or a great fight) for our flag! Proud of every single one of you, Nakama! 🚩",
+  "💤 Even the strongest pirates need sleep. The war is over. Rest well, the sea calls tomorrow! 🌙"
+];
+
+// END MESSAGES (23:00)
+const ghostLastCall = [
+  "⏰ ONLY 1 HOUR LEFT! Stop procrastinating, Nakama! Get the Ghost Trial done! 👻",
+  "🏃‍♂️ MOVE IT! Only 60 minutes left for the Ghost Trial! Don't let rewards vanish! 🌊",
+  "🏴‍☠️ 'A pirate never misses his mark!' - Finish the Ghost Trial NOW! ⚔️",
+  "💀 YOHOHOHO! Time is running out for your shadows! 1 hour left! 🕯️"
+];
+
+// START MESSAGES (10:00)
+const ghostStart = [
+  "💀 YOHOHOHO! A new day begins and the Ghost Trial is waiting for us! Don't let them take your shadows! 👻",
+  "🧟‍♂️ Thriller Bark vibes! Ghost Trial is open for the day. Let's show these spirits our crew's power! ⚔️",
+  "🌑 Wake up, Nakama! The spirits are restless. Ghost Trial is available, go clear it! 🕯️",
+  "🎻 The Binks' Sake is playing... the ghosts are calling! Time to hit the Ghost Trial! 💀",
+  "👻 Don't wait until the last minute! The Ghost Trial is open. Get those rewards now! 🔥"
+];
+
+ 
+
 module.exports = [
   { cron: "0 12 * * 2,4", message: "Shandora is open!", target: "everyone" },
   { cron: "0 12 * * 2,5", message: "Belly Rush today!", target: "everyone" },
   { cron: "0 12 * * 3,5,0", message: "Mania today!", target: "@everyone" },
-  { cron: "45 21 * * 3,5,0", message: "15 min until Mania starts!", target: "@everyone" },
-  { cron: "0 22 * * 3,5,0", message: "Mania is open!", target: "@everyone" },
+  { cron: "45 19 * * 3,5,0", message: "15 min until Mania starts!", target: "@everyone" },
+  { cron: "0 20 * * 3,5,0", message: "Mania is open!", target: "@everyone" },
 
 
   //SHANDORA !!!
@@ -18,13 +67,22 @@ module.exports = [
   // Ще изпраща съобщението в 12:00, 16:00 и 20:00 във вторник (2) и петък (5)
   { cron: "0 12,16,20 * * 2,5", message: "Belly Rush today!", target: "everyone"},
 
-  //GUILD WARS !!!
-  // 1. ПРЕП ХАЙП (11:00) – Време е за подготовка
-{ cron: "0 11 * * 1,3,5", message: "⚔️ Sharpen your blades and chug those potions! Preparation has started. Don't be the one showing up with a wooden sword! 🛡️", target: "everyone" },
-// 2. БОЕН ВИК (12, 15, 18:00) – Екшън фаза
-{ cron: "0 12,15,18 * * 1,3,5", message: "🔥 THE HORN HAS SOUNDED! Guild war is LIVE! Get in there and show them why we're the legends. Don't make us come find you! 🏹", target: "everyone" },
-// 3. ПОБЕДНО ПИТИЕ (21:00) – Край на войната
-{ cron: "0 21 * * 1,3,5", message: "🍻 Sheathe your weapons, heroes! The war has ended. Thanks for the carry (and the support)! Time to hit the tavern. 🏆", target: "everyone" }
+  // --- GUILD WARS (With Random Messages) !!!
+  { cron: "0 11 * * 1,3,5", message: () => random(warHype), target: "everyone" },
+  { cron: "0 12,15,18 * * 1,3,5",message: () => random(warBattle), target: "everyone" },
+  { cron: "0 21 * * 1,3,5", message: () => random(warEnd), target: "everyone" },
 
+  //Ghost Trial !!!
+  // GHOST TRIAL (Last hour reminder - Sunday 23:00)
+  { cron: "0 23 * * 0", message: () => random(ghostLastCall), target: "everyone" },
+  // GHOST TRIAL (START reiminder - everyday at 10:00)
+  { cron: "0 10 * * *", message: () => random(ghostStart),target: "everyone" },
+  // SPECIAL FOR HOSTED 
+  { cron: "0 10 * * *", message: "The event has started! Get your ass in the Ghost Trial! 💀", target: "@Hosted" },
+
+
+
+
+  
 
 ];
