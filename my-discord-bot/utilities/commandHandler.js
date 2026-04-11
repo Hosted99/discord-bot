ec8ed5b1382625ba57cdf083dd591a8a26388b20
Hosted99 <140538111+Hosted99@users.noreply.github.com>
2026-04-11 11:38:39 +0300

    commandHandler.js

--- my-discord-bot/utilities/commandHandler.js
+++ my-discord-bot/utilities/commandHandler.js
@@ -103,6 +103,7 @@ async function handleCommands(msg, pool) {
                 { name: "✨ Extras", value: hero.extras || "N/A", inline: false },
                 { name: "🍎 Devil Fruit", value: hero.devil_fruit || "N/A", inline: false },
                 { name: "🍊 Secondary", value: hero.secondary_fruit || "N/A", inline: false }
+                { name: "🌊 Fruit Awakenings", value: hero.awakenings || "N/A", inline: false }
             );
 
         return msg.channel.send({ embeds: [embed] });
