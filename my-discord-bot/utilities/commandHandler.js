099a13c90b1f54618ac147c11edcea341b5a7d25
Hosted99 <140538111+Hosted99@users.noreply.github.com>
2026-04-16 20:31:31 +0300

    commandHandler.js

--- my-discord-bot/utilities/commandHandler.js
+++ my-discord-bot/utilities/commandHandler.js
@@ -52,8 +52,8 @@ async function handleCommands(msg, pool) {
                 },
                 {
                     name: "🎖️ Role Management (Admin)", 
-                    value: "• `!addrole @user <Role>` - Assign crew role.\n• + 
-                        "• ` !removerole @user <Role>` - Remove crew role.\n" +
+                    value: "• `!addrole @user <Role>` - Assign crew role.\n•" + 
+                        "• ` !removerole @user <Role>` - Remove crew role.\n•" +
                      "• `!addroleallts @role` — (Admin) Add role to everyone with **ᐪˢ☠️**.\n" + 
                        "• `!addroleallgm @role` — (Admin) Add role to everyone with **ᴳᴹ☠️**."
 },
