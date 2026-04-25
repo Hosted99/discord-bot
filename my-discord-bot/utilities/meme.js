52b6b6367727daaf79c51cefba1e8c126c93536d
Hosted99 <140538111+Hosted99@users.noreply.github.com>
2026-04-25 16:04:35 +0300

    meme.js

--- my-discord-bot/utilities/meme.js
+++ my-discord-bot/utilities/meme.js
@@ -2,43 +2,37 @@ const { EmbedBuilder } = require('discord.js');
 
 module.exports = {
     async getRandomMeme(msg) {
-        // Списък с добри източници за мемета
-        const subreddits = ['memes', 'dankmemes', 'wholesomememes'];
-        const randomSub = subreddits[Math.floor(Math.random() * subreddits.length)];
+        // Списък с най-добрите места за мемета
+        const subs = ['memes', 'dankmemes', 'wholesomememes', 'PrequelMemes'];
+        const randomSub = subs[Math.floor(Math.random() * subs.length)];
 
         try {
-            // Опитваме се да вземем меме
-            const response = await fetch(`https://meme-api.com{randomSub}`);
-            
-            if (!response.ok) throw new Error('API Down');
-
+            // Взимаме мемета директно от JSON-а на Reddit (много по-стабилно)
+            const response = await fetch(`https://reddit.com{randomSub}/random/.json`);
             const data = await response.json();
 
-            const embed = new EmbedBuilder()
-                .setTitle(data.title)
-                .setURL(data.postLink)
-                .setImage(data.url)
-                .setColor('#ff4500')
-                .setFooter({ text: `👍 ${data.ups} | r/${data.subreddit}` });
+            // Reddit връща списък, взимаме първия пост
+            const post = data[0].data.children[0].data;
 
-            // ВАЖНО: Използваме channel.send, за да не гърми, ако командата е изтрита
+            // Проверка дали постът има картинка
+            if (!post.url || post.is_video) {
+                return this.getRandomMeme(msg); // Ако е видео или текст, опитай пак
+            }
+
+            const embed = new EmbedBuilder()
+                .setTitle(post.title.length > 256 ? post.title.substring(0, 253) + '...' : post.title)
+                .setURL(`https://reddit.com${post.permalink}`)
+                .setImage(post.url)
+                .setColor('#ff4500')
+                .setFooter({ text: `👍 ${post.ups} | Source: r/${post.subreddit}` });
+
             await msg.channel.send({ embeds: [embed] });
-            
-            // Трием !meme командата чак след като сме пратили мемето
             await msg.delete().catch(() => {});
 
         } catch (err) {
-            console.error("Meme Error:", err.message);
-            
-            // Ако първият сайт не работи, пробваме втори (алтернативно API)
-            try {
-                const altRes = await fetch('https://herokuapp.com');
-                const altData = await altRes.json();
-                
-                await msg.channel.send(altData.url);
-            } catch (e) {
-                await msg.channel.send("🌊 Even the ocean is dry today. No memes found, try again in a bit!");
-            }
+            console.error("Reddit Meme Error:", err.message);
+            // Ако и това се провали, пращаме едно гарантирано меме, за да не е празно
+            await msg.channel.send("https://imgflip.com"); 
         }
     }
 };
