84fda4fc376d2c4a41070865ba0142d6335abb20
Hosted99 <140538111+Hosted99@users.noreply.github.com>
2026-04-25 16:03:19 +0300

    meme.js

--- my-discord-bot/utilities/meme.js
+++ my-discord-bot/utilities/meme.js
@@ -1,25 +1,44 @@
-const axios = require('axios');
 const { EmbedBuilder } = require('discord.js');
 
 module.exports = {
     async getRandomMeme(msg) {
+        // Списък с добри източници за мемета
+        const subreddits = ['memes', 'dankmemes', 'wholesomememes'];
+        const randomSub = subreddits[Math.floor(Math.random() * subreddits.length)];
+
         try {
-            // Използваме стабилно API за мемета от Reddit
-            const response = await axios.get('https://meme-api.com');
-            const data = response.data;
+            // Опитваме се да вземем меме
+            const response = await fetch(`https://meme-api.com{randomSub}`);
+            
+            if (!response.ok) throw new Error('API Down');
+
+            const data = await response.json();
 
             const embed = new EmbedBuilder()
-                .setTitle(data.title) // Заглавието на мемето
-                .setURL(data.postLink) // Линк към оригиналния пост
-                .setImage(data.url) // Самата картинка
-                .setColor('#ff4500') // Оранжев цвят (като Reddit)
-                .setFooter({ text: `👍 ${data.ups} upvotes | Source: r/${data.subreddit}` });
+                .setTitle(data.title)
+                .setURL(data.postLink)
+                .setImage(data.url)
+                .setColor('#ff4500')
+                .setFooter({ text: `👍 ${data.ups} | r/${data.subreddit}` });
 
-            await msg.reply({ embeds: [embed] });
+            // ВАЖНО: Използваме channel.send, за да не гърми, ако командата е изтрита
+            await msg.channel.send({ embeds: [embed] });
+            
+            // Трием !meme командата чак след като сме пратили мемето
+            await msg.delete().catch(() => {});
+
         } catch (err) {
-            console.error("Грешка при меме командата:", err.message);
-            msg.reply("❌ Oops! The meme treasury is empty right now. Try again later!");
+            console.error("Meme Error:", err.message);
+            
+            // Ако първият сайт не работи, пробваме втори (алтернативно API)
+            try {
+                const altRes = await fetch('https://herokuapp.com');
+                const altData = await altRes.json();
+                
+                await msg.channel.send(altData.url);
+            } catch (e) {
+                await msg.channel.send("🌊 Even the ocean is dry today. No memes found, try again in a bit!");
+            }
         }
     }
 };
-
