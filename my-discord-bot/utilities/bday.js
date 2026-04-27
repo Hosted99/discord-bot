b73ceb9d432259913a24999e979b952f434736a3
Hosted99 <140538111+Hosted99@users.noreply.github.com>
2026-04-28 02:21:12 +0300

    bday.js

--- my-discord-bot/utilities/bday.js
+++ my-discord-bot/utilities/bday.js
@@ -3,9 +3,9 @@ const { EmbedBuilder } = require('discord.js');
 
 module.exports = (client) => {
     // ТЕСТ: Нагласено за 02:20 (след 4 минути)
-    cron.schedule('20 02 * * *', async () => {
-        const CHANNEL_ID = '1490378124259758261';
-        const FRIEND_ID = '190189929316352000';
+    cron.schedule('30 08 * * *', async () => {
+        const CHANNEL_ID = '1486343047632523398';
+        const FRIEND_ID = '1366783021193367633';
 
         try {
             const channel = await client.channels.fetch(CHANNEL_ID);
@@ -13,7 +13,7 @@ module.exports = (client) => {
                 .setColor('#00FFFF')
                 .setTitle('🎉 HAPPY BIRTHDAY! 🎉')
                 .setDescription(`Wishing <@${FRIEND_ID}> an incredible day! 🎂`)
-                .setImage('https://giphy.com');
+                .setImage('https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExYTVpbHliempjZWdmN3YzNDdvODFicWI0MG1vMWw4c2VpMmg3YThzdyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/YuKRFGvBhcSLVFO6Oh/giphy.gif');
 
             await channel.send({ content: `📢 @everyone, birthday time! <@${FRIEND_ID}> 🎈`, embeds: [bdayEmbed] });
             console.log("🚀 TEST SUCCESSFUL: Message sent.");
